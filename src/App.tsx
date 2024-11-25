0a3d0e951086bfbc5e0f407ad19dc2a94582d1c2
ebordenave <edward.bordenave@gmail.com>
2024-11-24 23:43:47 -0500

    add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+// @ts-ignore
 import React from 'react';
+import { Button } from './components/ui/button';
 
 const App: React.FC = () => {
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
   return (
     <div>
       <h1>Hello, Vite + React!</h1>
+      <Button onClick={handleClick} variant="default">Click Me</Button>
     </div>
   );
 };
